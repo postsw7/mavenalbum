25b00e97986eaca201ebcc805bedb164974075b7
Siwoo <postsw7@gmail.com>
2019-06-02 16:50:16 -0400

    Update Navbar with hooks props and useRef

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,44 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useRef } from 'react';
 
-const Navbar = ({ users }) => {
+const Navbar = ({
+  users,
+  albumsLen,
+  photosLen,
+  allAlbumsLen,
+  isPhotoView,
+  isUserSelected,
+  handleClickOnAlbumView,
+  handleClickOnPhotoView,
+  getAlbumsOfUser,
+}) => {
+  const selectEl = useRef(null);
   return (
     <nav className='level u-p-25'>
       <div className='level-left'>
         <div className='level-item'>
           <p className='subtitle is-5'>
-            <strong>{'100'}</strong> {'albums'}
+            <strong>
+              {isPhotoView
+                ? photosLen
+                : isUserSelected
+                ? albumsLen
+                : allAlbumsLen}
+            </strong>{' '}
+            {isPhotoView ? 'photos' : 'albums'}
           </p>
         </div>
         <div className='level-item'>
           <div className='field has-addons'>
             <div className='control'>
               <div className='select'>
-                <select defaultValue={'Select User'}>
+                <select
+                  ref={selectEl}
+                  defaultValue={'Select User'}
+                  onChange={({ target: { value } }) => {
+                    getAlbumsOfUser(value);
+                  }}
+                >
                   <option disabled hidden>
                     Select User
                   </option>
@@ -36,12 +60,12 @@ const Navbar = ({ users }) => {
       </div>
 
       <div className='level-right'>
-        <p className='level-item' onClick={() => {}}>
+        <p className='level-item' onClick={handleClickOnAlbumView}>
           <a className='button is-primary'>
             <strong>Albums</strong>
           </a>
         </p>
-        <p className='level-item' onClick={() => {}}>
+        <p className='level-item' onClick={handleClickOnPhotoView}>
           <a className='button is-success'>
             <strong>Photos</strong>
           </a>
