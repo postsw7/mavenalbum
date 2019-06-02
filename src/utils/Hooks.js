659361d39687362514e1e22130879c0c731207d2
Siwoo <postsw7@gmail.com>
2019-06-02 16:32:38 -0400

    Add useAppState and useAppContext hook

--- src/utils/Hooks.js
+++ src/utils/Hooks.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext, useMemo } from 'react';
 import API from './API';
 
 export function useUserList() {
@@ -28,3 +28,80 @@ export function useAlbumList() {
 
   return albums;
 }
+
+export const useUserName = userId => {
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    async function getUserName() {
+      const { data } = await API.get(`/users/${userId}`);
+      setUsername(data.username);
+    }
+    getUserName();
+  }, [userId]);
+
+  return username;
+};
+
+export const AppContext = React.createContext();
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+export const useAppState = () => {
+  const initialState = {
+    isPhotoView: null,
+    isUserSelected: null,
+    selectedAlbumPhotos: [],
+    selectedUserAlbums: [],
+  };
+
+  const [state, setState] = useState(initialState);
+
+  const actions = useMemo(() => getActions(setState), [setState]);
+
+  return { state, actions };
+};
+
+const getActions = (setState, initialState) => ({
+  handleClickOnPhotoView: () => {
+    setState(prevState => {
+      if (!prevState.isPhotoView) {
+        return {
+          ...prevState,
+          isPhotoView: true,
+        };
+      }
+      return prevState;
+    });
+  },
+  handleClickOnAlbumView: () => {
+    setState(prevState => {
+      if (prevState.isPhotoView) {
+        return {
+          ...prevState,
+          isPhotoView: false,
+        };
+      }
+      return prevState;
+    });
+  },
+  getPhotosOfAlbum: async albumId => {
+    const { data } = await API.get(`/photos?albumId=${albumId}`);
+    setState(prevState => ({
+      ...prevState,
+      isPhotoView: true,
+      selectedAlbumPhotos: data,
+    }));
+  },
+  getAlbumsOfUser: async userId => {
+    const { data } = await API.get(`/albums?userId=${userId}`);
+    setState(prevState => ({
+      ...prevState,
+      isPhotoView: false,
+      isUserSelected: true,
+      selectedUserAlbums: data,
+    }));
+  },
+});
