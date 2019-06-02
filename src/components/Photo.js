d3bddbb543ea92a73b76465e105455e46be72408
Siwoo <postsw7@gmail.com>
2019-06-02 16:54:26 -0400

    Update Photo component with hooks state

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Photo = () => {
+const Photo = ({ photo: { title, url, thumbnailUrl, albumId, id } }) => {
   return (
     <div className='tile is-child'>
       <article className='tile is-child is-info'>
         <figure className='image is-square'>
-          <img src={'src'} alt='user pic' />
+          <img src={thumbnailUrl} alt='user pic' />
         </figure>
-        <p className='title is-6'>{'title'}</p>
+        <p className='title is-6'>{title}</p>
       </article>
     </div>
   );
