d47f9b3466bedd28e373a085287a41bd731e68b7
Siwoo <postsw7@gmail.com>
2019-06-02 16:48:32 -0400

    Update Album component by using props from hooks

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { useAppContext, useUserName } from '../utils/Hooks';
 
-const Album = () => {
+const Album = ({ album: { id, title, userId }, getPhotosOfAlbum }) => {
+  const { isUserSelected } = useAppContext();
   return (
-    <div className='card'>
+    <div
+      className='card'
+      onClick={() => {
+        if (!isUserSelected) {
+          alert(
+            'You are not allowed to see photos of this album!\nPlease choose user of the album.'
+          );
+          return;
+        }
+        getPhotosOfAlbum(id);
+      }}
+    >
       <div className='card-content'>
-        <p className='title is-4'>{'title'}</p>
+        <p className='title is-4'>{title}</p>
       </div>
       <footer className='card-footer'>
         <p className='card-footer-item'>
           <span>
-            User ID: <span className='has-text-primary'>{'username'}</span>
+            User ID:{' '}
+            <span className='has-text-primary'>{useUserName(userId)}</span>
           </span>
         </p>
       </footer>
